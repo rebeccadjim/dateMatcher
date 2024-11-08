49ca11203029e5dea71b1d4a138f788c861d9aa3
rebeccadjim <117664113+rebeccadjim@users.noreply.github.com>
2024-11-07 21:00:55 -0300

    test sonar

--- date_matcher.py
+++ date_matcher.py
@@ -10,14 +10,27 @@ import plotly.graph_objects as go
 
 # Fonction pour pré-traiter les données
 def create_dataframe(category, file_path, date_column, sex_column, id_column):
-
+# Hello
     if file_path.endswith('.xlsx'):
-        dataframe = pd.read_excel(file_path)
-        dataframe = dataframe[[date_column, sex_column, id_column]]
-
+        try:
+            dataframe = pd.read_excel(file_path)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"Excel file '{file_path}' not found.")
     elif file_path.endswith('.csv'): 
-        dataframe = pd.read_csv(file_path)
-        dataframe[date_column] = pd.to_datetime(dataframe[date_column], errors='coerce')
+        try:
+            dataframe = pd.read_csv(file_path)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"CSV file '{file_path}' not found.")
+
+    # Check if the specified columns exist in the dataframe
+    if date_column not in dataframe.columns:
+        raise ValueError(f"Column '{date_column}' not found in the file.")
+    if sex_column not in dataframe.columns:
+        raise ValueError(f"Column '{sex_column}' not found in the file.")
+    if id_column not in dataframe.columns:
+        raise ValueError(f"Column '{id_column}' not found in the file.")
+
+    dataframe[date_column] = pd.to_datetime(dataframe[date_column], errors='coerce')
     
     final_dataframe = pd.DataFrame({
         'Category': [category] * len(dataframe[date_column]),
@@ -27,7 +40,6 @@ def create_dataframe(category, file_path, date_column, sex_column, id_column):
     })
 
     return final_dataframe
-
 # Fonction pour créer des csv
 def create_csv(csv_name, frame):
     with open(csv_name, 'w', newline='') as csv_file:
@@ -103,13 +115,19 @@ def add_matching_data_trace(category1, category2, fig, data, color, dash):
 
 # Collecte des paramètres nécessaire
 parser = argparse.ArgumentParser(description='Process some data files and columns.')
-parser.add_argument('--min_date', type=int, help='Minimum date difference for connections')
-parser.add_argument('--max_date', type=int, help='Maximum date difference for connections')
-parser.add_argument('--death_file', type=str, help='Path for deaths file')
-parser.add_argument('--inhumation_file', type=str, help='Path for inhumations file')
-parser.add_argument('--necropsy_file', type=str, help='Path for necropsies file')
+parser.add_argument('-min','--min_date', type=int, default=3, help='Minimum date difference for connections')
+parser.add_argument('-max','--max_date', type=int, default=6, help='Maximum date difference for connections')
+parser.add_argument('-d','--death_file', type=str, help='Path for deaths file')
+parser.add_argument('-i','--inhumation_file', type=str, help='Path for inhumations file')
+parser.add_argument('-n','--necropsy_file', type=str, help='Path for necropsies file')
 
 args = parser.parse_args()
+
+required_args = ['min_date', 'max_date', 'death_file', 'inhumation_file', 'necropsy_file']
+missing_args = [arg for arg in required_args if not getattr(args, arg)]
+
+if missing_args:
+    parser.error(f"The following required arguments are missing: {', '.join(missing_args)}")
 
 # Création des dataframes
 death_dataframe = create_dataframe('Death', args.death_file, 'FECHA_MUERTE', 'SEXO', 'ID_CNI')
